7484a52a278e23ba89c1424eb961e05c58d9453e
Daniel Dias <danieldias6946@gmail.com>
2021-10-14 11:56:32 -0300

    adicionando configurações para o heroku

--- index.js
+++ index.js
@@ -5,20 +5,22 @@ const database = require('./src/services/database');
 const cors = require('cors');
 const app = express();
 
+const PORT = process.env.PORT || 8000;
+
 const filmeRoutes = require('./src/routes/main.routes');
 const loginRoutes = require('./src/routes/usuarios.routes');
 const episodiosRoutes = require('./src/routes/episodios.routes');
 
 // middlewares
 app.use(bodyParser.json());
-app.use(cors())
+app.use(cors());
 app.use(morgan('dev'));
 
 // routes
-app.use('/',filmeRoutes);
-app.use('/usuario',loginRoutes);
-app.use('/episodio',episodiosRoutes);
+app.use('/', filmeRoutes);
+app.use('/usuario', loginRoutes);
+app.use('/episodio', episodiosRoutes);
 
-app.listen(8000,() => {
+app.listen(PORT, () => {
   console.log('Meu servidor está funcionando');
-})
+});
